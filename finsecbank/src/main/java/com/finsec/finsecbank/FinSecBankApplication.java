9c1ed3352e76223b4b9a06807dd53eb1d8d2cfe8
marioserban98 <serbanmariorobert98@gmail.com>
2025-02-22 13:08:55 +0200

    Create project with spring initialzr

---
+++ finsecbank/src/main/java/com/finsec/finsecbank/FinSecBankApplication.java
@@ -0,0 +1,13 @@
+package com.finsec.finsecbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSecBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSecBankApplication.class, args);
+	}
+
+}
---
+++ finsecbank/src/test/java/com/finsec/finsecbank/FinSecBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsec.finsecbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSecBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
